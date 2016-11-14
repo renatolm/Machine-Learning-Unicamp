8d38ecfb5bc56ff293109c5e1cc3c85fac2d3faa
renatolm <rlmoura@icloud.com>
2016-11-14 00:11:39 -0200

    fix typo

--- Exercicio5/ex5.py
+++ Exercicio5/ex5.py
@@ -101,7 +101,7 @@ gp_parameters = {'regr':['constant', 'linear', 'quadratic'],
 	'thetaL':[0.0001,0.001,0.01],
 	'thetaU':[0.1,1.0,10.0]}
 
-grid_gp = GridSeachCV(GaussianProcess(), gp_params, cv=3,
+grid_gp = GridSearchCV(GaussianProcess(), gp_params, cv=3,
 	scoring='neg_mean_absolute_error')
 grid_gp.fit(X_train, y_train)
 
