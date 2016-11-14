531b01f905e6db6debfd0aa61e1189a62e1ac8f3
renatolm <rlmoura@icloud.com>
2016-11-14 16:39:59 -0200

    making some tests on ex5

--- Exercicio5/ex5teste.py
+++ Exercicio5/ex5teste.py
@@ -18,6 +18,7 @@ data = pd.read_csv('train.csv', header=None)
 #Separando os valores a serem estimados do resto dos dados
 train_Y = data.pop(0)
 
+data = data.reset_index(drop=True)
 #Separando os dados numericos dos categoricos
 numericos = data.select_dtypes(include=['int64']).columns
 categoricos = data.select_dtypes(include=['object']).columns
