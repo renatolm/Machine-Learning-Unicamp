fb3e26611842cb673611bb9cf1a6af178231bb89
renatolm <rlmoura@icloud.com>
2016-11-16 21:34:46 -0200

    preprocessing done on ex6

--- Exercicio6/ex6.py
+++ Exercicio6/ex6.py
@@ -3,20 +3,26 @@ import pandas as pd
 import string
 from sklearn.datasets import load_files
 from sklearn.feature_extraction.text import CountVectorizer
+import re
+from nltk import PorterStemmer
+
+def removeNonAscii(s):
+	return re.sub(r'\\x\w{2}','',s)
 
 data = load_files('filesk1/')
-#print data
 
-categories = pd.read_csv('category.tab').values
-#print categories
+count_vect = CountVectorizer(stop_words='english')
 
-count_vect = CountVectorizer()
-data['data'] = str(data['data']).encode('UTF-8','strict')
-print data['data']
-data['data'] = str(data['data']).decode('UTF-8','strict')
-print data['data']
-data['data'] = str(data['data']).translate(string.maketrans("",""), string.punctuation)
 data['data'] = count_vect.build_preprocessor()(str(data['data']))
+data['data'] = removeNonAscii(data['data'])
 data['data'] = count_vect.build_tokenizer()(str(data['data']))
 data['data'] = count_vect.build_analyzer()(str(data['data']))
-#print data['data']
+
+stemmed_data = []
+for word in data['data']:
+	word = PorterStemmer().stem(word)
+	stemmed_data.append(word)
+
+data['data'] = stemmed_data
+
+print data['data']
